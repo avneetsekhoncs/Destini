c36fb325a729b99ad9c6c7d6a3ba3c2c9a7fc588
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-06-12 18:10:23 -0700

    Completed Project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let story0 : String = "You see a fork in the road."
+    let choice1 : String = "Take a left."
+    let choice2 : String = "Take a right."
+    var nextStory : Int = 0
+    
+    var storyBrain = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton)
+    {
+        
+        let userChoice = sender.currentTitle!
+        nextStory = storyBrain.nextStory(userChoice)
+        updateUI()
+    }
+    
+    func updateUI()
+    {
+        storyLabel.text = storyBrain.stories[nextStory].title
+        choice1Button.setTitle(storyBrain.stories[nextStory].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[nextStory].choice2, for: .normal)
+    }
 }
 
